23ade6084205a8f414bf0d9c51c3e387304e5502
WlanKasper <asu.artur.smirnov@gmail.com>
2022-05-06 11:09:10 +0200

    set

---
+++ scripts/script-0.js
@@ -0,0 +1,9 @@
+const elem = document.getElementById('text');
+const btn = document.querySelector('button');
+
+btn.onclick = function() {
+    elem.innerHTML = 'Hello world';
+}
+
+// Сделать калькулятор
+// Матрица кнопок как на калькуляторе
