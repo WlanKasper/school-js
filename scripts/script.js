e57c4a5b8e7548d16221f7783144c3598f19f14c
WlanKasper <asu.artur.smirnov@gmail.com>
2022-05-06 17:52:09 +0200

    new sfx

--- scripts/script.js
+++ scripts/script.js
@@ -3,23 +3,29 @@ const display = document.form.textview;
 let nextTimeClean = false;
 
 function insert(input) {
+    playSoundClickSmall();
+
     if (nextTimeClean) {
         clean();
         nextTimeClean = false;
     }
+
     display.value = display.value + input;
 }
 
 function clean() {
+    playSoundClickBig();
     display.value = '';
 }
 
 function back() {
+    playSoundClickBig();
     let exp = display.value;
     display.value = exp.substring(0, exp.length - 1);
 }
 
 function equal() {
+    playSoundClickBig();
     let exp = display.value;
     if (exp) {
         try {
@@ -30,4 +36,18 @@ function equal() {
             console.log(err);
         }
     }
+}
+
+async function playSoundClickSmall(){
+    let sound = new Audio();
+    sound.src = 'audio/click-small.mp3';
+    sound.volume = 0.2;
+    sound.autoplay = true;
+}
+
+async function playSoundClickBig(){
+    let sound = new Audio();
+    sound.src = 'audio/click-big.mp3';
+    sound.volume = 0.2;
+    sound.autoplay = true;
 }
