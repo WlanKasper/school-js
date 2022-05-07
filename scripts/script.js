c87937c993dd4402f890d157311f9ef4d84d5c3a
WlanKasper <asu.artur.smirnov@gmail.com>
2022-05-07 14:52:26 +0200

    fixed sound of clean btn after equal

--- scripts/script.js
+++ scripts/script.js
@@ -3,28 +3,30 @@ const display = document.form.textview;
 let nextTimeClean = false;
 
 function insert(input) {
+    console.log('New input: ' + input);
     playSoundClickSmall();
-
     if (nextTimeClean) {
-        clean();
+        clean(function () {});
         nextTimeClean = false;
     }
-
     display.value = display.value + input;
 }
 
-function clean() {
-    playSoundClickBig();
+function clean(playSound = () => playSoundClickBig()) {
+    console.log('Clearing all display...');
+    playSound();
     display.value = '';
 }
 
 function back() {
+    console.log('Clearing one simbol...');
     playSoundClickBig();
     let exp = display.value;
     display.value = exp.substring(0, exp.length - 1);
 }
 
 function equal() {
+    console.log('Calculating result...');
     playSoundClickBig();
     let exp = display.value;
     if (exp) {
@@ -38,16 +40,20 @@ function equal() {
     }
 }
 
-async function playSoundClickSmall(){
+async function playSoundClickSmall() {
+    console.log('Playing sound small...');
     let sound = new Audio();
     sound.src = 'audio/click-small.mp3';
     sound.volume = 0.2;
     sound.autoplay = true;
 }
 
-async function playSoundClickBig(){
+async function playSoundClickBig() {
+    console.log('Playing sound big...');
     let sound = new Audio();
     sound.src = 'audio/click-big.mp3';
     sound.volume = 0.2;
     sound.autoplay = true;
 }
+
+// function parsingDisplayData()
